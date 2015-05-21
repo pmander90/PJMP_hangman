aec9968eee3e513cf3d8842c00512bd9e8f71f1c
presto2116 <preston.straight@gmail.com>
2015-05-21 16:03:10 -0400

    added backbone files

---
+++ public/js/application.js
@@ -0,0 +1,9 @@
+//= require underscore
+//= require backbone
+//= require handlebars.runtime
+//= require_tree ./templates
+//= require_tree ./backbone/routers
+//= require_tree ./backbone/models
+//= require_tree ./backbone/collections
+//= require_tree ./backbone/views
+//= require_tree .
