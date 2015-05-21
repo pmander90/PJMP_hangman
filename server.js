5c9c8d9845c695e3e6dcfd4569117529bd5d0fb9
tsetoung <jtmoua@yahoo.com>
2015-05-21 17:11:29 -0400

    json request for words added (api)

--- server.js
+++ server.js
@@ -15,14 +15,18 @@ app.use(bodyParser.json());
 // starting db connection, schema, model
 mongoose.connect('mongodb://localhost/pjmp-hangman')
 var db= mongoose.connection
-var userSchema = mongoose.Schema({
-	name: String
-})
-var wordSchema = mongoose.Schema({
+var wordsSchema = mongoose.Schema({
 	word: String
 })
-var User = mongoose.model('user', userSchema)
-var Word = mongoose.model('word', wordSchema)
+var Word = mongoose.model('word', wordsSchema)
+Word.remove({}, function (err){
+	console.log("collection removed")
+})
+Word.create({word: "pizza"})
+Word.create({word: "zoo"})
+Word.create({word: "apple"})
+Word.create({word: "mongoose"})
+Word.create({word: "slice"})
 
 // setting up views and requiring public folder
 app.set("view engine", "hbs")
@@ -30,7 +34,14 @@ app.use(express.static(__dirname + "/public"))
 
 app.get("/", routes.index)
 
-
+app.get("/words", function(req, res){
+	var query = Word.find({})
+		query.select("-_id word")
+		query.exec(function(err, words){
+			res.setHeader("Content-Type", "application/json");
+			res.send(words)
+		})
+})
 
 
 
